b8142c5eb470690d19c6834d090ea2a19e791861
Liam Conrad <liam@w4pho.net>
2024-11-09 15:59:59 -0600

    appease the linter

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-#include <stdlib.h>
-#include "mbed_config.h"
+#include <cstdlib>
+//#include "mbed_config.h"
 
 struct boundaries_t {
   double f_min;
@@ -9,8 +9,8 @@ struct boundaries_t {
 
 boundaries_t b = {10, 100000};
 size_t num_bins = 4001;
-size_t max_bins = 4001;
-double* fourier_bin_frequencies = NULL;
+//size_t max_bins = 4001;
+double* fourier_bin_frequencies = nullptr;
 
 char* s = (char*) malloc(sizeof(char) * 256);
 
@@ -18,10 +18,10 @@ unsigned long start;
 unsigned long end;
 unsigned long iter = 0;
 
-double* generate_bins(const boundaries_t* b, int precision, size_t num_bins);
+double* generate_bins(const boundaries_t* bt, int precision, size_t n);
 double to_precision(double n, int places);
-double bin_frequency(int idx, const boundaries_t* b, int num_bins);
-double g(const boundaries_t* b);
+double bin_frequency(size_t idx, const boundaries_t* bt, size_t n);
+double g(const boundaries_t* bt);
 
 void setup() {
   Serial.begin(2000000);
@@ -52,32 +52,40 @@ void loop() {
   delay(60000); // min
 }
 
-double g(const boundaries_t* b) {
-  return log(b->f_max) - log(b->f_min);
+// g accepts the max and min fourier frequencies, and returns the difference between the logarithms of each.
+// The naming of this comes from the academic article "Improved spectrum estimation from..." cited in the references
+// section of README.md.
+double g(const boundaries_t* bt) {
+  return log(bt->f_max) - log(bt->f_min);
 }
 
-double bin_frequency(int idx, const boundaries_t* b, int num_bins) {
-  return b->f_min * exp((double(idx) * g(b))/double(num_bins-1));
+// bin_frequency is an implementation for determining the fourier frequency of a given bin on a logarithmic frequency
+// axis. This method was found in the academic article "Improved spectrum estimation from..." cited in the references
+// section of README.md.
+double bin_frequency(size_t idx, const boundaries_t* bt, size_t n) {
+  return bt->f_min * exp((double(idx) * g(bt))/double(n-1));
 }
 
+// to_precision rounds a number to the specified number of decimal places.
 double to_precision(double n, int places) {
   if (places == 0) {
     return n;
   }
-  float p = pow(10, places);
+  double p = pow(10, places);
   return round(n * p) / p;
 }
 
-double* generate_bins(const boundaries_t* b, int precision, size_t num_bins) {
+// generate_bins generates an array of logarithmic frequencies for the given boundaries and number of bins.
+double* generate_bins(const boundaries_t* bt, int precision, size_t n) {
   // At some point, disallow even number of bins
-  if (num_bins % 2 == 0) {
-    return NULL;
+  if (n % 2 == 0) {
+    return nullptr;
   }
 
-  double* v = new double[num_bins];
+  auto v = new double[n];
 
-  for (size_t i = 0; i < num_bins; i++) {
-    v[i] = to_precision(bin_frequency(i, b, num_bins), precision);
+  for (size_t i = 0; i < n; i++) {
+    v[i] = to_precision(bin_frequency(i, bt, n), precision);
   }
 
   return v;
